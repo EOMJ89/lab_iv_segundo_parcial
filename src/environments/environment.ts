1603328fdbae8a617eceac50de4f760a750b985e
EOMJ89 <emiliosegado14@gmail.com>
2019-10-18 21:53:26 -0300

    _

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDQk-eIpPnbJU5Rs5el5r5z-oFcxpUMfc0',
+    authDomain: 'lab-iv.firebaseapp.com',
+    databaseURL: 'https://lab-iv.firebaseio.com',
+    projectId: 'lab-iv',
+    storageBucket: 'lab-iv.appspot.com',
+    messagingSenderId: '826255913622',
+    appId: '1:826255913622:web:9962d89c3ae62c89'
+  },
 };
 
 /*
